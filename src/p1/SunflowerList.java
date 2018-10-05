a46363f79b954865a7d20035724b92638402c08d
María Arranz <mariaarranzl@gmail.com>
2018-10-05 07:43:02 +0000

    Update SunflowerList.java

--- src/p1/SunflowerList.java
+++ src/p1/SunflowerList.java
@@ -1,5 +1,23 @@
 package p1;
 
 public class SunflowerList {
-	private int[] lista;
+	private Sunflower[] lista;
+	private int cont;
+	
+	public SunflowerList(){
+		this.lista = new Sunflower[32] ;
+		this.cont = 0;
+	}
+	
+	public int length(){
+		return this.cont;
+	}
+	
+	public Sunflower lista(int pos){
+		return this.lista[pos];
+	}
+	public void addSunflower (int x, int y, Game juego){
+		this.lista[cont] = new Sunflower(x,y, juego);
+		++this.cont;
+	}
 }
