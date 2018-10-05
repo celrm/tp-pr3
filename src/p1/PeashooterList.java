7c3e9464198cecadbc0fe674c2662d89f7584eee
María Arranz <mariaarranzl@gmail.com>
2018-10-05 07:42:20 +0000

    Update PeashooterList.java

--- src/p1/PeashooterList.java
+++ src/p1/PeashooterList.java
@@ -1,5 +1,25 @@
 package p1;
 
 public class PeashooterList {
-	private int[] lista;
+	private Peashooter[] lista;
+	private int cont;
+	
+	public PeashooterList(){
+		this.lista = new Peashooter[32] ;
+		this.cont = 0;
+	}
+	
+	public int length(){
+		return this.cont;
+	}
+	
+	public Peashooter lista(int pos){
+		return this.lista[pos];
+	}
+	public void addPeashooter (int x, int y, Game juego){
+		this.lista[cont] = new Peashooter(x,y, juego);
+		++this.cont;
+	}
+	
+	
 }
