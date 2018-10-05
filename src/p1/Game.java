1a34da1cfe71d0693b540983498858f59dd3e76b
María Arranz <mariaarranzl@gmail.com>
2018-10-05 07:32:35 +0000

    Update Game.java

--- src/p1/Game.java
+++ src/p1/Game.java
@@ -12,6 +12,49 @@ public class Game {
 	private Random rand;
 	private Level level;
 	
+    public Game(Random ran, Level n) {
+		this.level = n;
+		this.rand = ran;
+		this.sunflowerList = new SunflowerList();
+		this.peashooterList = new PeashooterList();
+		this.zombieList = new ZombieList();
+		this.cycleCount = 0;
+		this.soles = new SuncoinManager();
+	}
+	
+	public int ciclos(){
+		return this.cycleCount;
+	}
+	
+	public int slength(){
+		return this.sunflowerList.length();
+	}
+	public int plength(){
+		return this.peashooterList.length();
+	}
+	public int zlength(){
+		return this.zombieList.length();
+	}
+	
+	public Peashooter p (int pos){
+		return this.peashooterList.lista(pos);
+	}
+	public Sunflower s (int pos){
+		return this.sunflowerList.lista(pos);
+	}
+	
+	public Zombie z(int pos){
+		return this.zombieList.lista(pos);
+	}
+	
+	public void cambiarSoles(int n){
+		this.soles.add(n);
+	}
+
+	public void draw(){
+		GamePrinter print = new GamePrinter(this, 4, 8);;
+		System.out.println(print);
+	}
 	public void update() {
 		for (int i = 0; i < sunflowerList.length(); ++i) {
 			if(sunflowerList.lista(i).vida()>0) sunflowerList.lista(i).generarSoles();
