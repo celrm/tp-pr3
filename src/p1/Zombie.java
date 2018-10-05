8f6a78d822e34ce68414391c315fb7add1d5708a
María Arranz <mariaarranzl@gmail.com>
2018-10-05 07:38:09 +0000

    Update Zombie.java

--- src/p1/Zombie.java
+++ src/p1/Zombie.java
@@ -5,4 +5,33 @@ public class Zombie {
 	private int y;
 	private int vida;
 	private Game juego;
+
+	private int nacimiento;
+	
+	public Zombie(int x, int y, Game juego){
+		this.x = x;
+		this.y = y;
+		this.vida = 5;
+		this.juego = juego;
+		this.nacimiento = juego.ciclos();
+	}
+	
+	public int vida(){
+		return this.vida;
+	}
+	public void serDanado(int dano){
+		this.vida -= dano;
+	}
+	
+	public void avanza(){
+		if (this.nacimiento % 2 == this.juego.ciclos() % 2){
+			-- this.x;
+		}
+	}
+	public int posx(){
+		return this.x;
+	}
+	public int posy(){
+		return this.y;
+	}
 }
