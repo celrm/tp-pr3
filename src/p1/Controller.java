65d7b5988d3abf2ad177c1893449fde1a0b13b66
Celia Rubio <celia_rubio@hotmail.es>
2018-10-05 07:58:04 +0000

    Update Controller.java

--- src/p1/Controller.java
+++ src/p1/Controller.java
@@ -14,11 +14,21 @@ public class Controller {
 	}
 	
 	public void run() {
-		while(this.game.isFinished){
+		while(!this.game.playerWins() || !this.game.zombiesWin()){
 			this.game.update();
 			this.game.draw();
 			this.game.command();
 			this.game.computer();
+		}
+		
+		if (this.game.playerWins()) {
+		    System.out.println("Player wins!");
+		}
+		else if (this.game.zombiesWin()) {
+		    System.out.println("Zombies win :(");
+		}
+		else {
+		    System.out.println("Chao");
 		}
 	}
 	
