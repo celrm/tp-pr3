6dd90d39d5e0d2c88fb5e87a87fab6cc482c9bb5
María Arranz <mariaarranzl@gmail.com>
2018-10-05 07:36:46 +0000

    Update Peashooter.java

--- src/p1/Peashooter.java
+++ src/p1/Peashooter.java
@@ -5,4 +5,27 @@ public class Peashooter {
 	private int y;
 	private int vida;
 	private Game juego;
+	
+	
+	public Peashooter(int x, int y, Game juego){
+		this.x = x;
+		this.y = y;
+		this.vida = 3;
+		this.juego = juego;
+	}
+	
+	public int vida(){
+		return this.vida;
+	}
+	public void serDanado(int dano){
+		this.vida -= dano;
+	}
+	
+	public int posx(){
+		return this.x;
+	}
+	
+	public int posy(){
+		return this.y;
+	}
 }
