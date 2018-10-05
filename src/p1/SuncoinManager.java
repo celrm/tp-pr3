212113560a6273b59d7713f77557854e1cbe3c63
María Arranz <mariaarranzl@gmail.com>
2018-10-05 08:29:58 +0000

    Update SuncoinManager.java

--- src/p1/SuncoinManager.java
+++ src/p1/SuncoinManager.java
@@ -1,5 +1,19 @@
 package p1;
 
 public class SuncoinManager {
-
+	
+	private int suncoins;
+	public static int INISUN = 50;
+	
+	public SuncoinManager(){
+		this.suncoins = INISUN;
+	}
+	
+	public int num (){
+		return this.suncoins;
+	}
+	
+	public void add(int n){
+		this.suncoins += n;
+	}
 }
