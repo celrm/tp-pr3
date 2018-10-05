2467d49751ce091ae505d45aa64b627203548dc7
María Arranz <mariaarranzl@gmail.com>
2018-10-05 17:16:18 +0000

    Update Level.java

--- src/p1/Level.java
+++ src/p1/Level.java
@@ -6,18 +6,18 @@ public enum  Level{
 	public int numZombies(){
 		int num = 0;
 		switch(this){
-		case EASY: num = 3;
-		case HARD:  num = 5;
-		case INSANE: num = 10;
+		case EASY: num = 3; break;
+		case HARD:  num = 5; break;
+		case INSANE: num = 10; break;
 		}
 		return num;
 	}
 	public double frec(){
 		double num = 0.0;
 		switch(this){
-		case EASY: num = 0.1;
-		case HARD:  num = 0.2;
-		case INSANE: num = 0.3;
+		case EASY: num = 0.1; break;
+		case HARD:  num = 0.2; break;
+		case INSANE: num = 0.3; break;
 		}
 		return num;
 	}
