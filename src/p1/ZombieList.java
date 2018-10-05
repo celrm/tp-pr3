ed2ed198eb181830eb488c5c9fcec936c5e24fd5
María Arranz <mariaarranzl@gmail.com>
2018-10-05 07:43:29 +0000

    Update ZombieList.java

--- src/p1/ZombieList.java
+++ src/p1/ZombieList.java
@@ -1,5 +1,23 @@
 package p1;
 
 public class ZombieList {
-	private int[] lista;
+	private Zombie[] lista;
+	private int cont;
+	
+	public ZombieList(){
+		this.lista = new Zombie[32] ;
+		this.cont = 0;
+	}
+	
+	public int length(){
+		return this.cont;
+	}
+	
+	public Zombie lista(int pos){
+		return this.lista[pos];
+	}
+	public void addZombie (int x, int y, Game juego){
+		this.lista[cont] = new Zombie(x,y, juego);
+		++this.cont;
+	}
 }
