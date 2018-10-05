df760f408f45de98dad0a5f51a83653bd2131c2a
Celia Rubio <celia_rubio@hotmail.es>
2018-10-05 07:41:24 +0000

    Update ZombieManager.java

--- src/p1/ZombieManager.java
+++ src/p1/ZombieManager.java
@@ -3,14 +3,17 @@ package p1;
 import java.util.Random;
 
 public class ZombieManager {
+    
 	private int numZombies;
 	private double frec;
 	private Random rnd;
+	
 	public ZombieManager(Level level){
 		this.numZombies = level.numZombies();
 		this.frec = level.frec();
 		this.rnd = new Random();
 	}
+	
 	public boolean isZombieAdded(Level level) {
 		Double d = rnd.nextDouble();
 		if (d < this.frec) {
@@ -20,5 +23,9 @@ public class ZombieManager {
 			return false;
 		}
 		
+	}
+	
+	public boolean remZombies() {
+	    return (numZombies == 0);
 	}
 }
