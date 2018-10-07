ec5697949cdbc17467bc39fcba88a1feff15b4a1
María Arranz Lobo <mariaarranzlobo@eduroam171037.eduroam.ucm.es>
2018-10-07 23:19:45 +0200

    I break nivel

--- src/p1/PlantsVsZombies.java
+++ src/p1/PlantsVsZombies.java
@@ -8,18 +8,16 @@ public class PlantsVsZombies {
 	public static void main(String[] args) {
 		long seed = (args.length > 1)?Long.parseLong(args[1]):new Random().nextInt(1000);
 
-		Random rnd = new Random(seed);
-		
+		Random rnd = new Random(seed);	
 		Level level;
-		
+
 		switch(args[0].toLowerCase()) {
-		case "easy" : level  = Level.EASY;
-		case "hard" : level = Level.HARD;
-		case "insane" : level = Level.INSANE;
-		default: level = Level.EASY;
+		case "easy" : level  = Level.EASY; break;
+		case "hard" : level = Level.HARD; break;
+		case "insane" : level = Level.INSANE;break;
+		default: level = Level.EASY; break;
 		}
-		
-		
+
 		Game juego = new Game(rnd, level);
 		Scanner sc = new Scanner(System.in);
 		Controller controlador = new Controller(juego, sc);
