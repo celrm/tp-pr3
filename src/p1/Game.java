9958350fb56d28655b5f422a069b58e6cb9c1246
Celia Rubio <celia_rubio@hotmail.es>
2018-10-08 10:21:39 +0200

    comentarios en game

--- src/p1/Game.java
+++ src/p1/Game.java
@@ -78,26 +78,25 @@ public class Game {
 	}
 	
 	public void draw(){
-		GamePrinter print = new GamePrinter(this, Game.DIMX, Game.DIMY);
-
 		System.out.println("Number of cycles: " + Integer.toString(this.ciclos));
 		System.out.println("Sun coins: " + Integer.toString(this.soles.num()));
 		System.out.println("Remaining Zombies: " + Integer.toString(this.zManager.remZombies()));
-		
+
+		GamePrinter print = new GamePrinter(this, Game.DIMX, Game.DIMY);		
 		System.out.println(print.toString());
 	}
 	
 	public void update() {
 		// crear soles
-		for (int i = 0; i < slength(); ++i) {
+		for (int i = 0; i < slength(); ++i)
 			if(s(i).vida()>0) s(i).generarSoles();
-		}
+		
 		//lanzar guisantes
 		for (int i = 0; i < plength(); ++i) {
 			if(p(i).vida()>0) {
 				boolean found = false;
 				for (int j = 0; j < zlength() && !found; ++j) {
-					if (z(j).vida() > 0 && p(i).posx() == z(j).posx()){
+					if (z(j).vida() > 0 && p(i).posx() == z(j).posx()) {
 						z(j).serDanado(Peashooter.HARM);
 						found = true;
 					}
@@ -109,6 +108,7 @@ public class Game {
 			if (z(i).vida()>0) {
 				boolean hayPlantas = false;
 				int j = 0;
+				//a sunflowers
 				while (j < slength() && !hayPlantas) {
 					hayPlantas = (s(j).vida() > 0) && (s(j).posy() == z(i).posy()-1 && s(j).posx() == z(i).posx());
 					++j;
@@ -117,81 +117,91 @@ public class Game {
 				else {
 					hayPlantas = false;
 					j = 0;
+					//a peashooters
 					while (j < plength() && !hayPlantas) {
 						hayPlantas = (p(j).vida() > 0) && (p(j).posy() == z(i).posy()-1 && p(j).posx() == z(i).posx());
 						++j;
 					}
+					
 					if (hayPlantas) p(j-1).serDanado(1);
-					else {
-					
-					hayPlantas = false;
-					j = 0;
-					while (j < zlength() && !hayPlantas) {
-						hayPlantas = (z(j).vida()> 0) && (z(j).posy() == z(i).posy()-1 && z(j).posx() == z(i).posx());
-						++j;
-					}
-					if (!hayPlantas) z(i).avanza();
-					
+					else {					
+						hayPlantas = false;
+						j = 0;
+						//comprobar si hay zombies delante
+						while (j < zlength() && !hayPlantas) {
+							hayPlantas = (z(j).vida()> 0) && (z(j).posy() == z(i).posy()-1 && z(j).posx() == z(i).posx());
+							++j;
+						}
+						if (!hayPlantas) z(i).avanza();
 					}
 				}
 			}
 		}
 	}
 	
-	public boolean playerWins(){
+	//gana el jugador si
+	public boolean playerWins() {
 	    boolean sol = false;
-        if(this.zManager.remZombies() == 0){
+	    
+	    //no quedan zombies por salir
+        if(this.zManager.remZombies() == 0) {
             sol = true;
-            for (int i = 0; i < this.zlength() && sol; ++i){
-                if (this.z(i).vida() > 0){
-                    sol = false;
-                }
-            }
+            
+            //y todos están muertos
+            for (int i = 0; i < this.zlength() && sol; ++i)
+                if (this.z(i).vida() > 0) sol = false;
         }
         return sol;
-
 	}
 	
-	public boolean zombiesWin(){
+	//ganan los zombies si
+	public boolean zombiesWin() {
 	    boolean sol = false;
-	    for (int i = 0; i < this.zlength() && !sol; ++i){
-	        if (this.z(i).posy() == 0){
-	            sol = true;
-	        }
+	    
+	    //de entre los zombies
+	    for (int i = 0; i < this.zlength() && !sol; ++i) {
+	    	//hay uno en la columna 0
+	        if (this.z(i).posy() == 0) sol = true;
 	    }
 	    return sol;
 	}
 	
+	//se añaden zombies
 	public void computer() {
-    //int x = Math.abs(this.rand.nextInt() % Game.DIMX);
 		boolean posible = false;
-		for (int i = 0; i < Game.DIMX && !posible; ++i){
+		//si hay algún hueco en la columna DIMY - 1
+		for (int i = 0; i < Game.DIMX && !posible; ++i) {
 			posible = !this.hayCosas(i, Game.DIMY - 1);
 		}
-	
-		if (posible && this.zManager.isZombieAdded()){
-			int x = Math.abs(this.rand.nextInt() % Game.DIMX);
+		//y si toca que salga en este ciclo
+		if (posible && this.zManager.isZombieAdded()) {
+			//aleatoriamente busco una fila libre
+			int x;
+			do x = Math.abs(this.rand.nextInt() % Game.DIMX);
+			while (this.hayCosas(x, Game.DIMY-1));
 			
-			while (this.hayCosas(x, Game.DIMY-1)){
-				x = Math.abs(this.rand.nextInt() % Game.DIMX);
-			}
+			//y añado al zombie
 			this.zombieList.addZombie(x, Game.DIMY-1, this);
 	    }	
 	}
 	
+	//función auxiliar para saber si un hueco está libre
 	private boolean hayCosas(int x, int y) {
 		boolean hayCosas = false;
 		int j = 0;
+		//miramos en la lista de sunflowers
 		while (j < slength() && !hayCosas) {
 			if (s(j).vida()>0 && s(j).posx()==x && s(j).posy()==y) hayCosas = true;
 			j++;
 		}
 		j = 0;
+		//en la lista de peashooters
 		while (j < plength() && !hayCosas) {
 			if (p(j).vida()>0 && p(j).posx()==x && p(j).posy()==y) hayCosas = true;
 			j++;
 		}
 		j = 0;
+		//y en la lista de zombies
 		while (j < zlength() && !hayCosas) {
 			if (z(j).vida()>0 && z(j).posx()==x && z(j).posy()==y) hayCosas = true;
 			j++;
@@ -199,12 +209,18 @@ public class Game {
 		return hayCosas;
 	}
 	
+	//compramos una planta
 	public boolean addPlant(String planta, int x, int y) {
 		boolean sol = false;
+		
+		//si no hay nada donde queremos colocarla
 		if(this.hayCosas(x,y)) System.out.println("There's already something there.");
+		//y si la posición está dentro del tablero
 		else if(x<0 || y<0 || x>=Game.DIMX || y>=Game.DIMY) System.out.println("Wrong position.");
 		else {
+			// si es una sunflower
 			if(planta.equals("s") || planta.equals("sunflower")) {
+				//si da el dinero
 				if (this.soles.num() >= Sunflower.COSTE) {
 					this.sunflowerList.addSunflower(x, y, this);
 					this.cambiarSoles(-Sunflower.COSTE);
@@ -212,7 +228,9 @@ public class Game {
 				}
 				else System.out.println("Not enough cash.");
 			}
+			//o un peashooter
 			else if(planta.equals("p") || planta.equals("peashooter")) {
+				//si da el dinero
 				if (this.soles.num() >= Peashooter.COSTE) {
 					this.peashooterList.addPeashooter(x, y, this);
 					this.cambiarSoles(-Peashooter.COSTE);
@@ -220,6 +238,7 @@ public class Game {
 				}
 				else System.out.println("Not enough cash.");
 			}
+			//y si no te equivocas de planta
 			else System.out.println("Wrong plant.");
 		}
 		return sol;
