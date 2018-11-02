9791a4bf23999af165718fa8b47a7cc870c9ad34
Celia Rubio <celia_rubio@hotmail.es>
2018-11-02 15:57:58 +0000

    Update AddCommand.java

--- src/p2/AddCommand.java
+++ src/p2/AddCommand.java
@@ -4,7 +4,7 @@ public class AddCommand extends Command {
 	private String plantName;
 	private int x;
 	private int y;
-	
+
 	public AddCommand(String plant, int x, int y) {
 		super("add", "[A]dd", "add flower");
 		this.plantName = plant;
@@ -41,19 +41,23 @@ public class AddCommand extends Command {
 
 	@Override
 	public Command parse(String[] commandWords, Controller controller) {
-		if (commandWords.length != 4) System.out.println("Wrong parameters.");
-		else if(commandWords[0]) {
-			int x = Integer.parseInt(commandWords[2]);
-			int y = Integer.parseInt(commandWords[3]);
-			
-			if(x<0 || y<0 || x>=Game.DIMX || y>=Game.DIMY) System.out.println("Wrong position.");
-			
-			else {
-				Command com = new AddCommand(commandWords[1], x, y);
-				return com;
-			}
-		}
-		else return null;
-	}
+		if(commandWords[0] != this.commandText)
+			return null;
 
+		if (commandWords.length != 4) {
+			System.out.println("Wrong parameters.");
+			return null;
+		}
+
+		int x = Integer.parseInt(commandWords[2]);
+		int y = Integer.parseInt(commandWords[3]);
+
+		if(x<0 || y<0 || x>=Game.DIMX || y>=Game.DIMY) {
+			System.out.println("Wrong position.");
+			return null;
+		}
+
+		Command com = new AddCommand(commandWords[1], x, y);
+		return com;
+	}
 }
