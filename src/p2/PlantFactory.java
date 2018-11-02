bf828814b2de34b5bc0c910ac4c2dbefb497e592
María Arranz Lobo <mariaarranzlobo@eduroam170241.eduroam.ucm.es>
2018-11-02 10:56:48 +0100

    tengo miedo

---
+++ src/p2/PlantFactory.java
@@ -0,0 +1,10 @@
+package p2;
+
+public class PlantFactory {
+		private static Plant[] availablePlants = {
+		};
+		public static Plant getPlant(String plantName){
+		}
+		public static String listOfAvilablePlants() {
+		}
+}
